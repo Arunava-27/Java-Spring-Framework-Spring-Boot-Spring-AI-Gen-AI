9ab128cca335586ad66584d1a36631d9f3dfce97
Arunava-27 <drarunkundu22@gmail.com>
2026-03-26 13:33:05 +0530

    Add .gitignore and update Hello class to calculate and print sum of two numbers

--- Hello.java
+++ Hello.java
@@ -1,6 +1,10 @@
 public class Hello {
     public static void main(String args[])
     {
-        System.out.println("\nHello World");
+        int num1 = 3;
+        int num2 = 5;
+        int tot = num1 + num2;
+
+        System.out.println(tot);
     }
 }
