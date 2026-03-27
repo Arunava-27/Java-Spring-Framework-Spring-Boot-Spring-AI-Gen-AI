9b48f33efdc5b0c53f2fdb65d7e27046c2b1f98f
Arunava-27 <drarunkundu22@gmail.com>
2026-03-27 12:37:36 +0530

    Add ConditionalStatements class to demonstrate conditional logic in Java

---
+++ ConditionalStatements.java
@@ -0,0 +1,11 @@
+public class ConditionalStatements {
+    public static void main(String[] args) {
+         int x = 28;
+
+         if (x>10 && x<=20) { // valid range 11 - 20
+            System.out.println("Hello 😊");
+         } else {
+            System.out.println("Bye! 🤞");
+         }
+    }
+}
