2566ec059a2ce4b991324f4b0ac50671c38fab28
Arunava-27 <drarunkundu22@gmail.com>
2026-03-27 13:59:20 +0530

    Add Exercise class to demonstrate conditional logic and grading system

--- Exercise.java
+++ Exercise.java
@@ -1,12 +1,27 @@
 public class Exercise {
+   public static void main(String[] args) {
+       
+        int score = 75;
+        char grade;
+        
+        // TODO: Check pass or fail
+        if (score >= 50) {
+          System.out.println("Passed");
+        } else {
+          System.out.println("Failed");
+        }
 
-  public static void main(String[] args){
-      
-       int age = 25;
-       double height = 5.9;
-       String name = "Alex";
+        // TODO: Determine grade based on score
+        if (score >= 90) {
+          grade = 'A';
+        } else if (score >= 75 && score <=89) {
+          grade = 'B';
+        } else if (score >= 60 && score <= 74) {
+          grade = 'C';
+        } else {
+          grade = 'D';
+        }
        
-       System.out.println("Name: "+name+",Age: "+age+",Height: "+height);
-       
-  }
+        System.out.println("Grade: "+grade);
+   }
 }
